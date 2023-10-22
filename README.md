27bff83c0d4d644d355cda777af1247e5cbb194e
sym33 <murat.sariyar@bfh.ch>
2023-10-22 14:45:42 +0200

    Full commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # PharmCatWrapper_CGM
+This repository includes 3 central folders for embedding PharmCAT:
++ DemoApplication: Java frontend for using PGx guideline information that is extracted and condensed via PharmCAT
++ phoenix: The backend for the clinical information system CGM Phoenix (proprietary)
++ VCFAnalyzer: wrapper for PharmCAT that allows to parse HL7 messages
+
 ![Figure of the wrapper](images/Wrapper_CGM.png)
--- target/test-classes/resetTestStructure.bat
+++
@@ -1,8 +0,0 @@
-echo %~dp0
-set current_dir = %~dp0
-move /-y "%current_dir%out\0000001.vcf" "%current_dir%vcf\0000001.vcf"
-move /-y "%current_dir%out\0000002.vcf" "%current_dir%vcf\0000002.vcf"
-move /-y "%current_dir%out\0000001.hl7" "%current_dir%orders\0000001.hl7"
-move /-y "%current_dir%out\0000002.hl7" "%current_dir%orders\0000002.hl7"
-del "%current_dir%out\0000001.vcf.report.html"
-del "%current_dir%out\0000002.vcf.report.html"
